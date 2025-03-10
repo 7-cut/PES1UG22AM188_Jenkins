7684a9e11cad89b8e1fceff34d90c0e35a75fe3f
7-cut <119734514+7-cut@users.noreply.github.com>
2025-03-10 20:52:58 +0530

    Update hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -5,6 +5,6 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-  cout << "I have successfully built and run this  \n";
+  cout << "I have successfully built a webhook and learnt how to automate using jenkins!!!!  \n";
   return 0;
 }
